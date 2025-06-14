d3fb1304e819a2afc677ed46ebcf60211b022f0f
vjiayreddy <13086943+vjiayreddy@users.noreply.github.com>
2025-06-14 06:03:20 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import BannerComponent from "@/components/shared/Banner";
+import KeyFeatures from "@/components/shared/KeyFeature";
+import HowToWork from "@/components/shared/HowToWork";
+
+
+export default function Example() {
+  return (
+    <>
+      <BannerComponent />
+      <HowToWork/>
+      <KeyFeatures />
+    </>
+  );
+}
